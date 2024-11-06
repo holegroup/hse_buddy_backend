f9e91c2d539bdd6e1fa55abae8b9a6db672ad1ed
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-11-06 14:00:55 +0530

    create the api for fetching data of all inspection forms

--- controllers/inspectionFromController.js
+++ controllers/inspectionFromController.js
@@ -1,10 +1,10 @@
-const inspectionForm = require("../models/inspectionform.model"); 
+const InspectionForm = require("../models/inspectionform.model"); 
 
 async function createInspectionForm(req, res){ 
     try{ 
         const { equip_name_look, date_manufacture, part_num, serial_num, maintenance_freq, equip_desc, picture } = req.body; 
 
-        const newInspection = new inspectionForm({ 
+        const newInspection = new InspectionForm({ 
             equip_name_look,
             date_manufacture,
             part_num,
@@ -28,5 +28,89 @@ async function createInspectionForm(req, res){
 }
 
 
+async function getAllInspectionForms(req, res) { 
+    try{ 
+        const inspections = await InspectionForm.find(); 
+        if(inspections.length === 0){ 
+            return res.status(404).json({ message: "There Are no Inspections Found"}); 
+        }
+        return res.status(200).json({ 
+            message: "Success", 
+            data: inspections,
+        })
+    }catch(e){ 
+        return res.status(500).json({message: e.message}); 
+    }
+}
 
-module.exports = {createInspectionForm}; 
+
+// // Get a single inspection form by part number
+// exports.getInspectionFormByPartNum = async (req, res) => {
+//     try {
+//         const { part_num } = req.params;
+//         const inspection = await Inspection.findOne({ part_num });
+
+//         if (!inspection) {
+//             return res.status(404).json({ message: 'Inspection form not found' });
+//         }
+
+//         return res.status(200).json({
+//             message: 'Inspection form fetched successfully',
+//             data: inspection
+//         });
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({ message: 'Server error', error: err });
+//     }
+// };
+
+// // Update an inspection form by part number
+// exports.updateInspectionForm = async (req, res) => {
+//     try {
+//         const { part_num } = req.params;
+//         const updatedData = req.body;
+
+//         const updatedInspection = await Inspection.findOneAndUpdate(
+//             { part_num },
+//             { $set: updatedData },
+//             { new: true } // Return the updated document
+//         );
+
+//         if (!updatedInspection) {
+//             return res.status(404).json({ message: 'Inspection form not found' });
+//         }
+
+//         return res.status(200).json({
+//             message: 'Inspection form updated successfully',
+//             data: updatedInspection
+//         });
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({ message: 'Server error', error: err });
+//     }
+// };
+
+// // Delete an inspection form by part number
+// exports.deleteInspectionForm = async (req, res) => {
+//     try {
+//         const { part_num } = req.params;
+
+//         const deletedInspection = await Inspection.findOneAndDelete({ part_num });
+
+//         if (!deletedInspection) {
+//             return res.status(404).json({ message: 'Inspection form not found' });
+//         }
+
+//         return res.status(200).json({
+//             message: 'Inspection form deleted successfully',
+//             data: deletedInspection
+//         });
+//     } catch (err) {
+//         console.error(err);
+//         return res.status(500).json({ message: 'Server error', error: err });
+//     }
+// };
+
+
+
+module.exports = {createInspectionForm, getAllInspectionForms}; 
--- routes/inspectionFormRoutes.js
+++ routes/inspectionFormRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express"); 
 router = express.Router(); 
-const {createInspectionForm} = require("../controllers/inspectionFromController"); 
+const {createInspectionForm, getAllInspectionForms} = require("../controllers/inspectionFromController"); 
 
 router.post("/submit-inspection-form", createInspectionForm); 
+router.get("/get-all-inspections", getAllInspectionForms); 
 
 module.exports = router
