5b4bd817ada9d2ddebc1bf66d136b313114aa7e1
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-11-06 12:07:39 +0530

    change the login method to post req

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ router = express.Router();
 const {login, createUser} = require("../controllers/userController"); 
 const {authMiddleware,superAdminMiddleware} = require("../middlewares/authMiddleware"); 
 
-router.get("/login", login); 
+router.post("/login", login); 
 router.post("/create-user",authMiddleware,superAdminMiddleware,createUser); 
 
 
