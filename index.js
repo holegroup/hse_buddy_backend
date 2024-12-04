3a4c66dc6d5413d1a6109091d2b85be18832cbd2
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-12-04 11:51:36 +0530

    change the scheduling time

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/api/tasks", taskRoutes);
 
 
 // job scheduling (update task status)
-schedule.scheduleJob("0 0 * * *", async () => { 
+schedule.scheduleJob("55 11 * * *", async () => { 
     await updateTaskStatus(); 
 }); 
 
