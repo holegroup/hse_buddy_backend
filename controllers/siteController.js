ed5107a9e38f487a3b584f9be3dc1da31d3e32b9
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-12-16 12:39:29 +0530

    added the fetch sites and fetch product apis

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -143,8 +143,48 @@ async function addPartsToSite(req, res){
   }
 }
 
+async function fetchAllSites(__, res) { 
+  try { 
+      const result = await Site.find().select("site_name location");
+
+      if(!result) { 
+          return res.status(404).json({message: "No Products Found"}); 
+      }
+
+      const sitesWithLocationString = result.map(site => {
+          const location = site.location;
+          const locationString = `${location.address}, ${location.city}, ${location.state}, ${location.country}- ${location.zip_code}`;
+          return {...site.toObject(), location: locationString};
+      });
+
+      return res.status(200).json({message: "Saved Sites", data: sitesWithLocationString }); 
+
+  } catch(e) { 
+      return res.status(500).json({message: e.message}); 
+  }
+}
+
+async function fetchProducts(req, res) {
+  try {
+      const id = req.params.id;
+
+      const result = await (Site.find({_id: id})).populate("products_stored.product_id").select("products_stored");
+
+      if(!result) { 
+          return res.status(404).json({message: "No Products Found"}); 
+      }
+
+      return res.status(200).json({message: "Saved Sites", data: result});   
+
+  } catch (e) {
+      return res.status(500).json({message: e.message}); 
+  }
+  
+}
+
+
 
 //   need to make an api for approved and disapproved
  
 
-module.exports = {createSite, addItemsToSite, addPartsToSite}; 
+module.exports = {createSite, addItemsToSite, addPartsToSite, fetchAllSites, fetchProducts}; 
--- routes/siteRoute.js
+++ routes/siteRoute.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 router = express.Router(); 
 
-const {createSite, addItemsToSite, addPartsToSite} = require("../controllers/siteController"); 
+const {createSite, addItemsToSite, addPartsToSite, fetchAllSites, fetchProducts} = require("../controllers/siteController"); 
 
 router.post("/create-site", createSite); 
 router.post("/add-items-site", addItemsToSite); 
 router.post("/add-parts-items", addPartsToSite); 
+router.get("/fetch-all-sites", fetchAllSites);
+router.get("/fetch-products/:id", fetchProducts); 
 
 module.exports = router; 
